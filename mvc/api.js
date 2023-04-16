dc61272d5591894a42c41951d63f781b0ac905f8
mliew2 <mliew2@uic.edu>
2023-04-16 18:46:43 -0400

    Change to double quotation for consistency

--- mvc/api.js
+++ mvc/api.js
@@ -1,9 +1,9 @@
 export const APIs = (() => {
-    const baseUrl = 'http://localhost:3000';
-    const todoPath = 'todos'
+    const baseUrl = "http://localhost:3000";
+    const todoPath = "todos"
 
     const createTodo = (newTodo) => {
-        return fetch([baseUrl, todoPath].join('/'), {
+        return fetch([baseUrl, todoPath].join("/"), {
             method: "POST",
             body: JSON.stringify(newTodo),
             headers: { "Content-Type": "application/json" },
@@ -11,21 +11,21 @@ export const APIs = (() => {
     };
 
     const deleteTodo = (id) => {
-        return fetch([baseUrl, todoPath, id].join('/'), {
+        return fetch([baseUrl, todoPath, id].join("/"), {
             method: "DELETE",
         }).then((res) => res.json());
     };
 
     const getTodos = () => {
-        return fetch([baseUrl, todoPath].join('/')).then((res) => res.json());
+        return fetch([baseUrl, todoPath].join("/")).then((res) => res.json());
     };
 
     const updateTodo = (updatedTodo, id) => {
-        return fetch([baseUrl, todoPath, id].join('/'), {
-            method: 'PUT',
+        return fetch([baseUrl, todoPath, id].join("/"), {
+            method: "PUT",
             body: JSON.stringify(updatedTodo),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8',
+                "Content-type": "application/json; charset=UTF-8",
             },
         }).then((res) => res.json());
     };
