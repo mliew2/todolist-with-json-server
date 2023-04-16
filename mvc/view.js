71427b30f57a43f3be250ec595b20d785d8158b2
mliew2 <mliew2@uic.edu>
2023-04-16 19:18:47 -0400

    Change layout to two lists - completed and pending

--- mvc/view.js
+++ mvc/view.js
@@ -1,18 +1,41 @@
 export const View = (() => {
-    const todolistEl = document.querySelector(".todo-list");
+    const pendinglistEl = document.querySelector(".pending-list");
+    const completedListEl = document.querySelector(".completed-list");
     const submitBtnEl = document.querySelector(".submit-btn");
     const inputEl = document.querySelector(".input");
 
     const renderTodos = (todos) => {
-        let todosTemplate = "";
+        let pendingTemplate = "";
+        let completedTemplate = "";
         todos.forEach((todo) => {
-            const liTemplate = `<li><span>${todo.content}</span><button class="delete-btn" id="${todo.id}">delete</button></li>`;
-            todosTemplate += liTemplate;
+            if (!todo.completed) {
+                const liTemplate = `
+                <li>
+                    <span>${todo.content}</span>
+                    <button class="edit-btn" id="${todo.id}">edit</button>
+                    <button class="delete-btn" id="${todo.id}">delete</button>
+                    <button class="complete-btn" id="${todo.id}">complete</button>
+                </li>
+                `;
+                pendingTemplate += liTemplate;
+            } else {
+                const liTemplate = `
+                <li>
+                    <button class="complete-btn" id="${todo.id}">complete</button>
+                    <span>${todo.content}</span>
+                    <button class="edit-btn" id="${todo.id}">edit</button>
+                    <button class="delete-btn" id="${todo.id}">delete</button>
+                </li>
+                `;
+                completedTemplate += liTemplate;
+            }
         });
         if (todos.length === 0) {
-            todosTemplate = "<h4>no task to display!</h4>";
+            pendingTemplate = "<h4>no task to display!</h4>";
+            completedTemplate = "<h4>no task to display!</h4>";
         }
-        todolistEl.innerHTML = todosTemplate;
+        pendinglistEl.innerHTML = pendingTemplate;
+        completedListEl.innerHTML = completedTemplate;
     };
 
     const clearInput = () => {
@@ -24,6 +47,7 @@ export const View = (() => {
         submitBtnEl,
         inputEl,
         clearInput,
-        todolistEl
+        pendinglistEl,
+        completedListEl
     };
 })();
