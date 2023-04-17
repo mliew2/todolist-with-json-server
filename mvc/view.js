1cd219393abe94e038ad13647a41f802ccc21a1f
mliew2 <mliew2@uic.edu>
2023-04-16 22:28:48 -0400

    Added styling and Icons for buttons

--- mvc/view.js
+++ mvc/view.js
@@ -8,15 +8,21 @@ export const View = (() => {
         let pendingTemplate = "";
         let completedTemplate = "";
         todos.forEach((todo) => {
-            const completeButton = `<button class="complete-btn" id="${todo.id}">complete</button>`
+            const editIcon ='<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>';
+            const deleteIcon ='<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>';
+            const arrowLeftIcon ='<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg>';
+            const arrowRightIcon ='<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>';
+            
             const liTemplate = `
                 <span id="todo-${todo.id}">${todo.content}</span>
-                <button class="edit-btn" id="${todo.id}">edit</button>
-                <button class="delete-btn" id="${todo.id}">delete</button>
+                <button class="edit-btn" id="${todo.id}">${editIcon}</button>
+                <button class="delete-btn" id="${todo.id}">${deleteIcon}</button>
             `;
             if (!todo.completed) {
+                const completeButton = `<button class="complete-btn" id="${todo.id}">${arrowRightIcon}</button>`
                 pendingTemplate += `<li> ${liTemplate} ${completeButton} </li>`;
             } else {
+                const completeButton = `<button class="complete-btn" id="${todo.id}">${arrowLeftIcon}</button>`
                 completedTemplate += `<li> ${completeButton} ${liTemplate} </li>`;;
             }
         });
